0c9091dd1266258a7eba9914b039808e10443b35
Naren <alekhyabonkuri96@gmail.com>
2024-07-17 17:22:13 -0400

    text wrap

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 git pull
-ansible-playbook -i  $1.dev.alekhyab96.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
+ansible-playbook -i  frontend.dev.alekhyab96.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=frontend main.yml
+
+#you can use bash run.sh $1 in ansible putty terminal
